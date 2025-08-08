7698f83d8c8fbafe0881b2ed861826b125057c33
Stoyan Krastev (skkdevcraft) <163518570+skkdevcraft@users.noreply.github.com>
2025-08-08 08:14:11 +0300

    fix: call start-vnc.sh instead of start-vnc

--- test/xfce/empty-password.sh
+++ test/xfce/empty-password.sh
@@ -24,7 +24,7 @@ echo -e "password\npassword\nn" | tigervncserver -xstartup /usr/bin/xterm
 check "check Xtigervnc was started (1)" bash -c "ps aux | grep Xtigervnc"
 
 # Start the VNC server with XFCE
-start-vnc
+start-vnc.sh
 check "check Xtigervnc was started (2)" bash -c "ps aux | grep Xtigervnc"
 
 # Report results
